9ea5dd64f6f328c00e184508b8c7e101bff7a71e
JSE955 <jordanscottedwards@gmail.com>
2025-03-27 16:18:45 -0400

    Add basic object structure

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,17 @@
-console.log('Testing...');
+function GameBoard() {
+    const board = []
+    const getBoard = () => board;
+
+    return { getBoard }
+}
+
+function Player(name, mark) {
+    const getName = () => name;
+    const getMark = () => mark;
+
+    return { getName, getMark }
+}
+
+function GameController() {
+    
+}
