57f60c65c5d6a4fa4b9e4ced82ac116785f589e6
JSE955 <jordanscottedwards@gmail.com>
2025-03-27 17:12:00 -0400

    Build preliminary game loop

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,25 @@
 function GameBoard() {
-    const board = []
+    const board = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
     const getBoard = () => board;
 
-    return { getBoard }
+    const printBoard = () => {
+        console.log('-------------');
+        console.log(`| ${board[0]} | ${board[1]} | ${board[2]} |`);
+        console.log(`| ${board[3]} | ${board[4]} | ${board[5]} |`);
+        console.log(`| ${board[6]} | ${board[7]} | ${board[8]} |`);
+        console.log('-------------');
+    }
+
+    const addMark = (option, mark) => {
+        board.forEach((cell, index) => {
+            if (cell === option) {
+                board[index] = mark;
+            }
+        })
+    }
+
+    return { getBoard, printBoard, addMark }
 }
 
 function Player(name, mark) {
@@ -13,5 +30,29 @@ function Player(name, mark) {
 }
 
 function GameController() {
-    
+    const playerOne = Player('Player 1', 'X');
+    const playerTwo = Player('Player 2', 'O');
+    const board = GameBoard();
+    let currentPlayer = playerOne;
+
+    function switchCurrentPlayer() {
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    }
+    function getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    console.log('Welcome to Tic-Tac-Toe');
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getCurrentPlayer()}'s turn`);
+    }
+
+    function playRound(option) {
+        board.addMark(option, getCurrentPlayer().getMark());
+        // checkForWinner
+        printNewRound();
+    }
+
 }
